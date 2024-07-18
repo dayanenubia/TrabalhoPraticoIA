6bf90dbd158676ce4175578b1c5a881ac11903a8
Dayane Núbia <dayane.nubia67@gmail.com>
2024-07-18 19:58:18 -0300

    Add files via upload

---
+++ TrabalhoPraticoIA-main/src/trabalhoCargaAviao/Main.java
@@ -0,0 +1,22 @@
+package trabalhoCargaAviao;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int populacao = 20;
+        double limitePeso = 400; // Capacidade máxima de peso do avião em kg
+        double larguraMaxima = 300; // Largura máxima da área de carga em cm
+        double alturaMaxima = 300; // Altura máxima da área de carga em cm
+        double profundidadeMaxima = 400; // Profundidade máxima da área de carga em cm
+        int probabilidadeMutacao = 5;
+        int qtdCruzamento = 5;
+        int numeroGeracoes = 10;
+
+        AlgoritmoGenetico meuAg = new AlgoritmoGenetico(
+            populacao, limitePeso, larguraMaxima, alturaMaxima, profundidadeMaxima, probabilidadeMutacao, qtdCruzamento, numeroGeracoes
+        );
+
+        meuAg.carregaArquivo("carga_aviao.csv");
+        meuAg.executar();
+    }
+}
